b6b336ba1b64690c63c8af0c86eedef559b8ac5d
guillegomezz8 <guigomrom@alum.us.es>
2024-02-29 19:58:14 +0100

    SobreMi casi hecho, quedan redes Sociales

--- base/views.py
+++ base/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def base(request):
     return render(request, 'base.html')
+
+def sobreMi(request):
+    return render(request, 'sobreMi.html')

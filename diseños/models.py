a7183f070affc4370a259836b49a56cdbffb3ac5
guillegomezz8 <guigomrom@alum.us.es>
2024-03-01 09:45:40 +0100

    SobreMi acabado y Modelos diseño y imagen hechos

--- diseños/models.py
+++ diseños/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Imagen(models.Model):
+    image = models.ImageField(upload_to='images/')
+
+class Diseño(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=120)
+    imagenes = models.ManyToManyField(Imagen)

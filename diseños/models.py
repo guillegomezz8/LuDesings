d710466e2524c6531b1329e776cb64f24441bb18
guillegomezz8 <guigomrom@alum.us.es>
2024-03-05 14:19:43 +0100

    pagina de diseños okay

--- diseños/models.py
+++ diseños/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Imagen(models.Model):
-    image = models.ImageField(upload_to='images/')
+    imagen = models.ImageField(upload_to='images/')
 
 class Diseño(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=120)
+    nombre = models.CharField(max_length=50)
+    descripccion = models.CharField(max_length=120)
     imagen_principal = models.OneToOneField(Imagen, on_delete=models.CASCADE, related_name='imagen_principal_de',blank=True)
     imagenes_adicionales = models.ManyToManyField(Imagen, related_name='imagenes_adicionales_de')
 

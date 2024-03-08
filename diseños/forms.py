e1665a89744c9287acc82b4163de5a37edb7b793
guillegomezz8 <guigomrom@alum.us.es>
2024-03-08 17:46:00 +0100

    Error en el form

--- diseños/forms.py
+++ diseños/forms.py
@@ -5,3 +5,7 @@ class DiseñoForm(forms.ModelForm):
     class Meta:
         model = Diseño
         fields = ['nombre', 'descripccion', 'imagen_principal', 'imagenes_adicionales']
+
+    imagen_principal = forms.FileField(label='Imagen principal', widget=forms.FileInput(attrs={'accept': 'image/*'}))
+    imagenes_adicionales = forms.FileField(widget=forms.FileInput(attrs={'multiple': True, 'accept': 'image/*'}), label='Imágenes adicionales')
+
--- diseños/views.py
+++ diseños/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Diseño
+from .models import Diseño,Imagen
 from .forms import DiseñoForm
 
 # Create your views here.
@@ -8,11 +8,29 @@ def diseños(request):
     return render(request, 'diseños.html',{'diseños': diseños})
 
 def crear_diseño(request):
+    form = DiseñoForm(request.POST, request.FILES)
     if request.method == 'POST':
         form = DiseñoForm(request.POST, request.FILES)
         if form.is_valid():
-            diseño = form.save()
-            return redirect('detalle_diseño', pk=diseño.pk)  # Redirigir a la vista de detalle del diseño recién creado
+            nombre = form.cleaned_data['nombre']
+            descripcion = form.cleaned_data['descripccion']
+            imagen_principal = form.cleaned_data['imagen_principal']
+            imagenes_adicionales = request.FILES.getlist('imagenes_adicionales')
+
+            # Guardar imagen principal
+            imagen_principal_obj = Imagen.objects.create(imagen=imagen_principal)
+
+            # Crear diseño
+            diseño = Diseño.objects.create(nombre=nombre, descripccion=descripcion, imagen_principal=imagen_principal_obj)
+
+            # Guardar imágenes adicionales
+            for imagen_adicional in imagenes_adicionales:
+                imagen_adicional_obj = Imagen.objects.create(imagen=imagen_adicional)
+                diseño.imagenes_adicionales.add(imagen_adicional_obj)
+
+            return redirect('diseños')  # Redirigir a la vista de detalle del diseño recién creado
     else:
         form = DiseñoForm()
     return render(request, 'diseñoForm.html', {'form': form})
+
+

4e497a4f760c227e0ef742ccb7ad90e2f0fabd2f
guillegomezz8 <guigomrom@alum.us.es>
2024-03-14 15:57:52 +0100

    error multiplefileinput

--- diseños/forms.py
+++ diseños/forms.py
@@ -1,7 +1,25 @@
 from django import forms
 from .models import Diseño
 
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
+
 class DiseñoForm(forms.ModelForm):
+    imagenes_adicionales = MultipleFileField(required=False)  # Asigna el campo con el nuevo widget
+
     class Meta:
         model = Diseño
         fields = ['nombre', 'descripccion', 'imagen_principal', 'imagenes_adicionales']
@@ -16,5 +34,6 @@ class DiseñoForm(forms.ModelForm):
             'nombre': forms.TextInput(attrs={'class': 'form-control form-control-lg ', 'placeholder': 'Introduce el nombre'}),
             'descripccion': forms.TextInput(attrs={'class': 'form-control form-control-lg', 'placeholder': 'Introduce la descripción'}),
             'imagen_principal': forms.FileInput(attrs={'class': 'form-control form-control-lg', 'accept': 'image/*'}),
-            'imagenes_adicionales': forms.FileInput(attrs={'class': 'form-control form-control-lg', 'multiple': True, 'accept': 'image/*'}),
+            # Ahora usamos el nuevo widget MultipleFileInput
+            'imagenes_adicionales': MultipleFileInput(attrs={'class': 'form-control form-control-lg', 'accept': 'image/*'}),
         }
